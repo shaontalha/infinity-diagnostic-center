afdef9819a1a7c89e6b31a8ae5af16edd3e62ff0
shaontalha <shaontalha24@gmail.com>
2021-10-20 22:46:44 +0600

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import Home from './Pages/Home/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NotFound from './Pages/NotFound/NotFound';
+import Booking from './Pages/Booking/Booking/Booking';
+import Login from './Pages/Login/Login/Login';
+import Header from './Pages/Shared/Header/Header';
+import AuthProvider from './context/AuthProvider';
+import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
+import Register from './Pages/Register/Register';
+import Contact from './Pages/Contact/Contact';
+import Premium from './Pages/Premium/Premium';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthProvider>
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path='/home'>
+              <Home></Home>
+            </Route>
+            <Route path='/login'>
+              <Login></Login>
+            </Route>
+            <Route path='/register'>
+              <Register></Register>
+            </Route>
+            <PrivateRoute path='/booking/:serviceId'>
+              <Booking></Booking>
+            </PrivateRoute>
+            <PrivateRoute path='/contact'>
+              <Contact></Contact>
+            </PrivateRoute>
+            <PrivateRoute path='/premium'>
+              <Premium></Premium>
+            </PrivateRoute>
+            <Route path='*'>
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
